2a4fc671dcc2502c8c5ce2edbab742d9b5857e42
npelland <nickrpelland@gmail.com>
2017-02-20 22:19:11 -0600

    Added simple GUI window with generate button, location drop down, and time dropdown

--- GUI.py
+++ GUI.py
@@ -1,16 +1,34 @@
 '''
-
+Simple GUI controller
 
 
 '''
+from tkinter import *
 
-
-import tkinter
-
-#create the window
 root = Tk()
+root.title("IOT Controller")
+root.geometry("600x200")
+topFrame = Frame(root)
+topFrame.pack()
+bottomFrame =  Frame(root)
+bottomFrame.pack(side=BOTTOM)
 
-#modify
-root.title("Ryan is a loser")
-root.geometry("200x200")
+
+button1 = Button(bottomFrame, text="Generate", fg='black')
+
+button1.pack()
+
+variable = StringVar(root)
+variable.set("Location") # default value
+
+w = OptionMenu(root, variable, "one", "two", "three")
+w.pack(side=LEFT)
+
+variable1 = StringVar(root)
+variable1.set("Date") # default value
+
+w = OptionMenu(root, variable1, "one", "two", "three")
+w.pack(side=LEFT)
+
+root.mainloop()
 
