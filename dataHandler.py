0aa1babdd848a671c5375b2d6ed688f87fd2fb99
Ryan <Ryan>
2017-03-08 16:15:16 -0600

    added functions data_handler

--- dataHandler.py
+++ dataHandler.py
@@ -1,13 +1,24 @@
 '''
 getTweets(location, time)
 
-getPhrase(location, time) -> 1-5 popular phrases at the time
+getRateLimitStatus(rateSection, rateItem) -> returns the remaining API calls for each different type of call
+    rateSection :: one of: 'users', 'statuses', 'help', 'search'
+    rateItem :: one of: third = ['profile_banner', 'suggestions/:slug/members', 'show/:id', 'suggestions', 'lookup', 'search',
+                                'contributors', 'contributees', 'suggestions/:slug', mentions_timeline', 'lookup','show/:id',
+                                'oembed', 'retweeters/ids', 'home_timeline', 'user_timeline','retweets/:id','retweets_of_me',
+                                'privacy', 'tos', 'configuration', 'languages','tweets']
+
+getTrendingTopics(location) -> returns a list of the top 10 trending topics for the region defined by the WOEID (Countries only)
+    location :: WOEID number for the country that is desired
+
+woeidLookup(location) -> returns the WOEID for the location parameter
+    location :: A string containing a location e.g. 'Spain' or 'Des Moines, Iowa' or 'Foz de Iguazu, Parana, Brazil'
 
 getUsers(userIds) -> returns a list of user objects corresponding to the IDs sent
     userIDs :: A list containing at least one twitter user ID
 '''
 
-import tweepy
+import tweepy, yweather
 
 consumer_key = 'H5jr0sAGtmHS2EYM9KMNJflNP'
 consumer_secret = 'X7rL5RcWIlfRHfQp1lr6AMOPE0ZGqIBdoaRCXTGmAPXGVmjpBW'
@@ -17,10 +28,7 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
-
-def getTweets(location, time):
-
-    return 0
+client = yweather.Client()
 
 def getUsers(userIDs):
     users = []
@@ -28,3 +36,28 @@ def getUsers(userIDs):
         users.append(api.get_user(user))
 
     return users
+
+def woeidLookup(location):
+    woeid = client.fetch_woeid(location)
+
+    return woeid
+
+def getTrendingTopics(location):
+    trends = api.trends_place(id=location)
+    data = trends[0]
+    trending = data['trends']
+    names = [trend['name'] for trend in trending]
+    trendsList = ' '.join(names).split()
+
+    return trendsList
+
+def getRateLimitStatus(rateSection, rateItem):
+    data = api.rate_limit_status()
+    # second = ['users', 'statuses', 'help', 'search']
+    # third = ['/users/profile_banner', '/users/suggestions/:slug/members', '/users/show/:id', '/users/suggestions', '/users/lookup', '/users/search', '/users/contributors', '/users/contributees', '/users/suggestions/:slug', '/statuses/mentions_timeline', '/statuses/lookup','/statuses/show/:id', '/statuses/oembed', '/statuses/retweeters/ids', '/statuses/home_timeline', '/statuses/user_timeline','/statuses/retweets/:id', '/statuses/retweets_of_me', '/help/privacy', '/help/tos', '/help/configuration', '/help/languages','/search/tweets']
+
+    return data['resources'][rateSection]['/' + rateSection + '/' + rateItem]['remaining']
+
+#print(getTrendingTopics(woeidLookup('United States')))
+#print(getRateLimitStatus('users', 'suggestions'))
+
